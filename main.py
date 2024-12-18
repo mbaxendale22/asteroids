954cf86261d1c7d2b47c6d3319e4ff92ad932bfa
Matthew Baxendale <mdbaxendale1@gmail.com>
2024-12-18 15:21:00 +0000

    complete player class

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
-    print("Starting asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        for u in updatable:
+            u.update(dt)
+
         screen.fill(color="black")
+        for d in drawable:
+
+            d.draw(screen)
+
+        # player.update(dt)
+        # player.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
